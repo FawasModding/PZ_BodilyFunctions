4cb0ba30033f1e537c4512b7d651b48d52c283fd
ethan <fawastudios@gmail.com>
2025-08-21 13:35:12 -0400

    Refactor soiled clothing overlays and script structure
    Split peed and pooped overlay item scripts into individual files under new Overlays directories, removing the old combined scripts. Updated Lua logic to use new overlay structure, unified minimum severity threshold, and removed unused dried overlay code.

--- Contents/mods/PZ_BodilyFunctions/42/media/lua/client/_Shared/Utils/BF_Overlays.lua
+++ Contents/mods/PZ_BodilyFunctions/42/media/lua/client/_Shared/Utils/BF_Overlays.lua
@@ -8,12 +8,12 @@ local function generateOverlayStates(baseName, includeSat)
             ["75"]  = baseName .. "_75",
             ["100"] = baseName .. "_100",
         },
-        dried = {
-            ["25"]  = baseName .. "_Dried_25",
-            ["50"]  = baseName .. "_Dried_50",
-            ["75"]  = baseName .. "_Dried_75",
-            ["100"] = baseName .. "_Dried_100",
-        }
+        --dried = {
+        --    ["25"]  = baseName .. "_Dried_25",
+        --    ["50"]  = baseName .. "_Dried_50",
+        --    ["75"]  = baseName .. "_Dried_75",
+       --     ["100"] = baseName .. "_Dried_100",
+        --}
     }
     if includeSat then
         overlays.fresh.sat = baseName .. "_Sat"
@@ -28,7 +28,7 @@ BF_Overlays = {
             types = {"Trousers_Black","Trousers_CamoDesert","Trousers_CamoDesertNew","Trousers_CamoGreen","Trousers_CamoMilius","Trousers_CamoTigerStripe","Trousers_CamoUrban","Trousers_Chef","Trousers_DeerHide","Trousers_Denim_Punk","Trousers_Denim","Trousers_FaunHide","Trousers_Fireman","Trousers_Hide","Trousers_HuntingCamo","Trousers_JeanBaggy_Punk","Trousers_JeanBaggy","Trousers_LeatherBlack","Trousers_LeatherCrafted","Trousers_NavyBlue","Trousers_OliveDrab","Trousers_Padded_HuntingCamo","Trousers_Padded","Trousers_Police","Trousers_PoliceGrey","Trousers_PrisonGuard","Trousers_Ranger","Trousers_Scrubs","Trousers_Shellsuit_Black", "Trousers_Shellsuit_Blue", "Trousers_Shellsuit_Green", "Trousers_Shellsuit_Pink", "Trousers_Shellsuit_Teal", "Trousers_Shellsuit_White", "Trousers_Sheriff", "Trousers_Sport", "Trousers_WhiteTINT", "Trousers", "TrousersMesh_DenimLight", "TrousersMesh_Leather"}, 
 
             overlays = {
-                pee = generateOverlayStates("BF.Trousers_Peed", true),
+                pee = generateOverlayStates("BF.Trousers_Peed", false),
                 poop = generateOverlayStates("BF.Trousers_Pooped", false),
             }
         },
@@ -36,7 +36,7 @@ BF_Overlays = {
             types = {"Trousers_Suit", "Trousers_SuitTEXTURE", "Trousers_SuitWhite", "Trousers_Jeans", "Trousers_Camo", "Trousers_Army", "Trousers_Crafted_Cotton", "Trousers_Crafted_Burlap"},
 
             overlays = {
-                pee = generateOverlayStates("BF.SuitTrousersMesh_Peed", true),
+                pee = generateOverlayStates("BF.SuitTrousersMesh_Peed", false),
                 poop = generateOverlayStates("BF.SuitTrousersMesh_Pooped", false),
             }
         },
@@ -44,7 +44,7 @@ BF_Overlays = {
             types = {"Boxers_White", "Male_Boxers_Pants_2", "Male_Boxers_Pants_3", "Boxers_Hearts", "Boxers_Silk_Black", "Boxers_Silk_Red", "Boxers_RedStripes", "Briefs_SmallTrunks_Black", "Briefs_SmallTrunks_Blue", "Briefs_SmallTrunks_Red", "Briefs_SmallTrunks_WhiteTINT", "Briefs_Garbage", "Briefs_Burlap", "Briefs_Denim", "Briefs_Hide", "Briefs_Rag", "Briefs_Tarp", "Briefs_White", "Briefs_AnimalPrints"},
 
             overlays = {
-                pee = generateOverlayStates("BF.Male_Boxers_Peed", true),
+                pee = generateOverlayStates("BF.Male_Boxers_Peed", false),
                 poop = generateOverlayStates("BF.Male_Boxers_Pooped", false),
             }
         },
@@ -52,7 +52,7 @@ BF_Overlays = {
             types = {"Underpants_White", "Bikini_TINT", "Underpants_Black", "Underpants_RedSpots", "Underpants_AnimalPrint", "Underpants_Hide", "FrillyUnderpants_Black", "FrillyUnderpants_Pink", "FrillyUnderpants_Red", "SwimTrunks_Blue", "SwimTrunks_Green", "SwimTrunks_Red", "SwimTrunks_Yellow", "Shorts_HockeyPants", "Shorts_HockeyPants_Black", "Shorts_HockeyPants_Red", "Shorts_HockeyPants_UniBlue", "Shorts_HockeyPants_White", "Shorts_FootballPants", "Shorts_FootballPants_Black", "Shorts_FootballPants_Gold", "Shorts_FootballPants_White"},
 
             overlays = {
-                pee = generateOverlayStates("BF.Female_Underpants_Peed", true),
+                pee = generateOverlayStates("BF.Female_Underpants_Peed", false),
                 poop = generateOverlayStates("BF.Female_Underpants_Pooped", false),
             }
         },
@@ -60,7 +60,7 @@ BF_Overlays = {
             types = {"Shorts_LongDenim", "Shorts_LongDenim_Punk", "Shorts_LongSport", "Shorts_LongSport_Red", "Shorts_CamoGreenLong", "Shorts_CamoUrbanLong", "Shorts_OliveDrabLong", "Shorts_CamoDesertNewLong", "Shorts_CamoMiliusLong", "Shorts_CamoTigerStripeLong"},
 
             overlays = {
-                pee = generateOverlayStates("BF.LongShorts_Peed", true),
+                pee = generateOverlayStates("BF.LongShorts_Peed", false),
                 poop = generateOverlayStates("BF.LongShorts_Pooped", false),
             }
         },
@@ -68,7 +68,7 @@ BF_Overlays = {
             types = {"Shorts_BoxingRed", "Shorts_BoxingBlue"},
 
             overlays = {
-                pee = generateOverlayStates("BF.BoxingShorts_Peed", true),
+                pee = generateOverlayStates("BF.BoxingShorts_Peed", false),
                 poop = generateOverlayStates("BF.BoxingShorts_Pooped", false),
             }
         },
@@ -76,7 +76,7 @@ BF_Overlays = {
             types = {"Shorts_ShortDenim", "Shorts_ShortFormal", "Shorts_ShortSport"},
 
             overlays = {
-                pee = generateOverlayStates("BF.ShortShorts_Peed", true),
+                pee = generateOverlayStates("BF.ShortShorts_Peed", false),
                 poop = generateOverlayStates("BF.ShortShorts_Pooped", false),
             }
         }
