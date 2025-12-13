e4b5211f243e1e3a8995b188cd2f2c84876aea77
ethan <fawastudios@gmail.com>
2025-12-13 16:04:10 -0500

    Fixed FecalFootprints tag

--- Contents/mods/PZ_BodilyFunctions/42/media/lua/client/_Shared/World/WasteProducts/BF_FecalFootprints.lua
+++ Contents/mods/PZ_BodilyFunctions/42/media/lua/client/_Shared/World/WasteProducts/BF_FecalFootprints.lua
@@ -75,8 +75,8 @@ BF_FecalFootprints.HasFecesOnSquare = function(square)
         local objectContainer = object:getContainer()
         if object and object:getObjectName() == "WorldInventoryItem" then
             local item = object:getItem()
-            if item and item:hasTag("BF:HumanFeces") then return true end
-        elseif objectContainer and objectContainer:getCountTagRecurse("BF:HumanFeces") > 0 then
+            if item and item:hasTag(BFTags.HumanFeces) then return true end
+        elseif objectContainer and objectContainer:getCountTagRecurse(BFTags.HumanFeces) > 0 then
             return true
         end
     end
